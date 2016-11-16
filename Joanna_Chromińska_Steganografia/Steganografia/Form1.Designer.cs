b80795828c16a5ecc776fcf29b0d8000c0eca0b3
joannach <joanna.chrominska9@gmail.com>
2016-11-16 12:59:51 +0100

    Add files via upload

---
+++ Joanna_Chromińska_Steganografia/Steganografia/Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Steganografia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_wczytaj1 = new System.Windows.Forms.Button();
+            this.button_zapisz1 = new System.Windows.Forms.Button();
+            this.button_wczytaj_tekst = new System.Windows.Forms.Button();
+            this.button_wczytaj2 = new System.Windows.Forms.Button();
+            this.button_ukryj = new System.Windows.Forms.Button();
+            this.button_odkryj = new System.Windows.Forms.Button();
+            this.button_zapisz2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_wczytaj1
+            // 
+            this.button_wczytaj1.Location = new System.Drawing.Point(12, 223);
+            this.button_wczytaj1.Name = "button_wczytaj1";
+            this.button_wczytaj1.Size = new System.Drawing.Size(75, 23);
+            this.button_wczytaj1.TabIndex = 0;
+            this.button_wczytaj1.Text = "Wczytaj";
+            this.button_wczytaj1.UseVisualStyleBackColor = true;
+            this.button_wczytaj1.Click += new System.EventHandler(this.button_wczytaj1_Click);
+            // 
+            // button_zapisz1
+            // 
+            this.button_zapisz1.Location = new System.Drawing.Point(183, 223);
+            this.button_zapisz1.Name = "button_zapisz1";
+            this.button_zapisz1.Size = new System.Drawing.Size(75, 23);
+            this.button_zapisz1.TabIndex = 1;
+            this.button_zapisz1.Text = "Zapisz";
+            this.button_zapisz1.UseVisualStyleBackColor = true;
+            this.button_zapisz1.Click += new System.EventHandler(this.button_zapisz1_Click);
+            // 
+            // button_wczytaj_tekst
+            // 
+            this.button_wczytaj_tekst.Location = new System.Drawing.Point(15, 288);
+            this.button_wczytaj_tekst.Name = "button_wczytaj_tekst";
+            this.button_wczytaj_tekst.Size = new System.Drawing.Size(106, 23);
+            this.button_wczytaj_tekst.TabIndex = 2;
+            this.button_wczytaj_tekst.Text = "Wczytaj tekst";
+            this.button_wczytaj_tekst.UseVisualStyleBackColor = true;
+            this.button_wczytaj_tekst.Click += new System.EventHandler(this.button_wczytaj_tekst_Click);
+            // 
+            // button_wczytaj2
+            // 
+            this.button_wczytaj2.Location = new System.Drawing.Point(264, 223);
+            this.button_wczytaj2.Name = "button_wczytaj2";
+            this.button_wczytaj2.Size = new System.Drawing.Size(75, 23);
+            this.button_wczytaj2.TabIndex = 3;
+            this.button_wczytaj2.Text = "Wczytaj";
+            this.button_wczytaj2.UseVisualStyleBackColor = true;
+            this.button_wczytaj2.Click += new System.EventHandler(this.button_wczytaj2_Click);
+            // 
+            // button_ukryj
+            // 
+            this.button_ukryj.Location = new System.Drawing.Point(167, 291);
+            this.button_ukryj.Name = "button_ukryj";
+            this.button_ukryj.Size = new System.Drawing.Size(75, 43);
+            this.button_ukryj.TabIndex = 4;
+            this.button_ukryj.Text = "Ukryj";
+            this.button_ukryj.UseVisualStyleBackColor = true;
+            this.button_ukryj.Click += new System.EventHandler(this.button_ukryj_Click);
+            // 
+            // button_odkryj
+            // 
+            this.button_odkryj.Location = new System.Drawing.Point(425, 291);
+            this.button_odkryj.Name = "button_odkryj";
+            this.button_odkryj.Size = new System.Drawing.Size(75, 43);
+            this.button_odkryj.TabIndex = 6;
+            this.button_odkryj.Text = "Odkryj";
+            this.button_odkryj.UseVisualStyleBackColor = true;
+            this.button_odkryj.Click += new System.EventHandler(this.button_odkryj_Click);
+            // 
+            // button_zapisz2
+            // 
+            this.button_zapisz2.Location = new System.Drawing.Point(436, 223);
+            this.button_zapisz2.Name = "button_zapisz2";
+            this.button_zapisz2.Size = new System.Drawing.Size(75, 23);
+            this.button_zapisz2.TabIndex = 7;
+            this.button_zapisz2.Text = "Zapisz";
+            this.button_zapisz2.UseVisualStyleBackColor = true;
+            this.button_zapisz2.Click += new System.EventHandler(this.button_zapisz2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 262);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(246, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(264, 262);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(246, 179);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.pictureBox2.Location = new System.Drawing.Point(264, 42);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(247, 179);
+            this.pictureBox2.TabIndex = 11;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Obraz bez ukrytej wiadomości";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(261, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Obraz z ukrytą wiadomością";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(267, 291);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Ilosc znakow:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(342, 288);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(43, 20);
+            this.textBox3.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 361);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button_zapisz2);
+            this.Controls.Add(this.button_odkryj);
+            this.Controls.Add(this.button_ukryj);
+            this.Controls.Add(this.button_wczytaj2);
+            this.Controls.Add(this.button_wczytaj_tekst);
+            this.Controls.Add(this.button_zapisz1);
+            this.Controls.Add(this.button_wczytaj1);
+            this.Name = "Form1";
+            this.Text = "Steganograf";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_wczytaj1;
+        private System.Windows.Forms.Button button_zapisz1;
+        private System.Windows.Forms.Button button_wczytaj_tekst;
+        private System.Windows.Forms.Button button_wczytaj2;
+        private System.Windows.Forms.Button button_ukryj;
+        private System.Windows.Forms.Button button_odkryj;
+        private System.Windows.Forms.Button button_zapisz2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Joanna_Chromińska_Steganografia/Steganografia/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.Threading;
+using System.IO;
+using System.Timers;
+using System.Diagnostics;
+
+namespace Steganografia
+{
+    public partial class Form1 : Form
+    {
+        int[,,][] t;
+        char[] text;
+        int[][] tab;
+        Bitmap wczytana, bmp;
+      //  int XY = 240;
+
+        byte zmien(byte k, int b)
+        {
+            if (k % 2 == 1)
+            {
+                if (b == 0)
+                    k--;
+            }
+            else
+            {
+                if (b == 1)
+                    k++;
+            }
+            return k;
+        }
+
+        private void button_wczytaj1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            string s = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Load(openFileDialog1.FileName);
+                s = openFileDialog1.FileName;
+            }
+
+            wczytana = new Bitmap(s);
+
+        }
+
+        private void button_zapisz1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+
+        private void button_wczytaj_tekst_Click(object sender, EventArgs e)
+        {
+            StreamReader sr;
+            StringBuilder sb = new StringBuilder();
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            string s;
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                sr = new StreamReader(openFileDialog1.FileName);
+                do
+                {
+                    s = sr.ReadLine();
+                    sb.Append(s + Environment.NewLine);
+                } while (s != null);
+
+                sr.Close();
+                textBox1.Text = sb.ToString();
+            }
+        }
+
+        void szyfruj(Bitmap w)
+        {
+            bmp = new Bitmap(w);
+            int ch = 0, bit = 0;
+            for (int i = 0; i < bmp.Width; i++)
+            {
+                for (int j = 0; j < bmp.Height; j++)
+                {
+                    if (ch < tab.Length)
+                    {
+                        bmp.SetPixel(i, j, Color.FromArgb(
+                            zmien(w.GetPixel(i, j).A, tab[ch][bit]),
+                            zmien(w.GetPixel(i, j).R, tab[ch][bit + 1]),
+                            zmien(w.GetPixel(i, j).G, tab[ch][bit + 2]),
+                            zmien(w.GetPixel(i, j).B, tab[ch][bit + 3])));
+                        bit += 4;
+
+                        if (bit == 8)
+                        {
+                            bit = 0;
+                            ch++;
+                        }
+                    }
+                    else
+                    {
+                        i = bmp.Width;
+                        j = bmp.Height;
+                    }
+                }
+            }
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.Image = bmp;
+
+        }
+
+        private void button_ukryj_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length <= (wczytana.Height * wczytana.Width) / 2)
+            {
+                string s;
+                int cc = 0;
+                text = textBox1.Text.Select(c => char.Parse(c.ToString())).ToArray();
+                tab = new int[text.Length][];
+
+                for (int i = 0; i < text.Length; i++)
+                {
+                    s = Convert.ToString(Convert.ToInt16(text[i]), 2);
+                    if (s.Length < 8)
+                    {
+                        cc = s.Length;
+                        while (8 - cc > 0)
+                        {
+                            s = 0 + s;
+                            cc++;
+                        }
+                    }
+                    tab[i] = s.Select(c => int.Parse(c.ToString())).ToArray();
+                }
+                szyfruj(wczytana);               
+
+            }
+            else
+            {
+                MessageBox.Show("Obraz nie zmieści tylu danych." + Environment.NewLine +
+                    "Maksymalna ilość znaków: " + (wczytana.Height* wczytana.Width) / 2);
+            }
+}
+
+        private void button_odkryj_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox3.Text) * 2 > bmp.Height * bmp.Width)
+            {
+                MessageBox.Show("Obraz nie mieści tylu danych." + Environment.NewLine +
+                    "Maksymalna ilość znaków: " + (bmp.Height * bmp.Width) / 2);
+            }
+            else
+            {
+                int y = Convert.ToInt32(textBox3.Text) * 2;
+                double z = (double)y / bmp.Height;
+                int x = (int)Math.Ceiling(z);
+                if (y > bmp.Height)
+                    y = bmp.Height;
+                if (x > bmp.Width)
+                    x = bmp.Width;
+                StringBuilder bulid = new StringBuilder();
+
+                int a = 0, p = 0;
+
+                for (int i = 0; i < x; i++)
+                {
+                    for (int j = 0; j < y; j++)
+                    {
+                        if ((j + p) % 2 == 0)
+                        {
+                            a += 64 * (bmp.GetPixel(i, j).R % 2);
+                            a += 32 * (bmp.GetPixel(i, j).G % 2);
+                            a += 16 * (bmp.GetPixel(i, j).B % 2);
+                        }
+                        else
+                        {
+                            a += 8 * (bmp.GetPixel(i, j).A % 2);
+                            a += 4 * (bmp.GetPixel(i, j).R % 2);
+                            a += 2 * (bmp.GetPixel(i, j).G % 2);
+                            a += 1 * (bmp.GetPixel(i, j).B % 2);
+
+                            bulid.Append((char)a);
+                            a = 0;
+                        }
+                    }
+                    if (i % 2 == 0 && bmp.Height % 2 == 1)
+                        p = 1;
+                    else
+                        p = 0;
+
+                    if (i + 1 == x - 1)
+                    {
+                        if (((Convert.ToInt32(textBox3.Text) * 2) % y) == 0)
+                        { }
+                        else
+                            y = (Convert.ToInt32(textBox3.Text) * 2) % y;
+                    }
+                }
+
+                textBox2.Text = Convert.ToString(bulid);
+            }
+        }
+
+        private void button_wczytaj2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            string s = "";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox2.Load(openFileDialog1.FileName);
+                s = openFileDialog1.FileName;
+            }
+
+            bmp = new Bitmap(s);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_zapisz2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(saveFileDialog1.FileName);
+            }
+        }
+    }
+}
